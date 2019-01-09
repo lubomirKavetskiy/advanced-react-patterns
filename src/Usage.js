f3c618896c538601f3c8da983e17781c7bb8f15b
lubomir <lubomirkavetskiy@gmail.com>
2019-01-09 06:57:39 +0200

    Validate Compound Component Context Consumer

--- src/Usage.js
+++ src/Usage.js
@@ -2,11 +2,29 @@ import React from 'react';
 
 import Switch from './Switch';
 
+const ToggleContext = React.createContext();
+
+//to sure that context does exist we use validation
+function ToggleConsumer(props) {
+  return (
+    <ToggleContext.Consumer>
+      {context => {
+        if (!context) throw new Error(
+          'Toggle-consumer of compoud compnents must be always rendered within the Toggle-provider'
+        );
+        console.log(props.children); //({ on }) => on ? children : null
+        console.log(context); //{on: false, onClick: f()}
+        return props.children(context);
+      }}
+    </ToggleContext.Consumer>
+  );
+}
+
 class Toggle extends React.Component {
-  static On = ({ on, children }) => (on ? children : null);
-  static Off = ({ on, children }) => (on ? null : children);
-  //static Button = ({ on, toggle, ...rest }) => <Switch on={on} onClick={toggle} {...rest} />;
-  static Button = (props) => <Switch {...props} />;
+  //static On = ({ children }) => <ToggleConsumer>{context => context.on ? children : null}</ToggleConsumer>;
+  static On = ({ children }) => <ToggleConsumer>{({ on }) => on ? children : null}</ToggleConsumer>;
+  static Off = ({ children }) => <ToggleConsumer>{({ on }) => on ? null : children}</ToggleConsumer>;
+  static Button = () => <ToggleConsumer>{props => <Switch {...props} />}</ToggleConsumer>;
 
   state = { on: false };
 
@@ -14,24 +32,36 @@ class Toggle extends React.Component {
 
   render() {
     return (
-      React.Children.map(this.props.children, child => {
-        return React.cloneElement(child, {
-          on: this.state.on,
-          onClick: this.toggle,
-        })
-      }
-
-      )
-    )
+      <ToggleContext.Provider
+        value={
+          {
+            on: this.state.on,
+            onClick: this.toggle,
+          }
+        }>
+        {this.props.children}
+      </ToggleContext.Provider>
+    );
   }
 }
 
 function Usage() {
   return (
+    //if use <div> instead of <Toggle> (Toggle-provider) we will not pass context, so it will be throw Error
     <Toggle>
       <Toggle.On>Button is on</Toggle.On>
       <Toggle.Off>Button is off</Toggle.Off>
-      <Toggle.Button />
+      <div>
+        <div>
+          <div>
+            <div>
+              <div>
+                <Toggle.Button />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </Toggle>
   );
 }
